f72d56229a920cbd5eebb3ceb70081dcdc4b57f5
t-yamashita <ymtk.0815@gmail.com>
2016-10-17 19:20:12 +0900

    3.13

---
+++ chapter3/13/13.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	f "fmt"
+	. "math"
+)
+
+func main() {
+	f.Println(Pi)
+
+	f.Println(AppVersion)
+	printMessage("Hello.")
+}
+
+
---
+++ chapter3/13/app.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	f "fmt"
+)
+
+const (
+	AppVersion = 1.2
+)
+
+func printMessage(s string) {
+	f.Println(s)
+}
